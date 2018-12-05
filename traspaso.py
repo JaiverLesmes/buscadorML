075fad8ec0e242bc0a23cf2003fa9daba537804e
Mike Brian Olivera <mikesolivera@gmail.com>
2018-12-05 15:19:29 -0500

    act

--- traspaso.py
+++ traspaso.py
@@ -1,7 +1,8 @@
-from bs4 import BeautifulSoup
+
 import re
 import os
-
+from pprint import pprint as pp
+from glob import glob
 import sqlite3
 
 stopWords = ['&#3','reuter','i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"]
@@ -20,6 +21,8 @@ c.execute('''CREATE TABLE IF NOT EXISTS noticias(id INTEGER PRIMARY KEY AUTOINCR
 
 
 listado = []
+diccionario = set()
+texts = ()
 
 def guardar(titulo,cuerpo):
     #csv.write(titulo+","+cuerpo+"\n")
@@ -73,6 +76,11 @@ def extraer_titulo_body(texto):
 
     body =" ".join(str(x) for x in filtered_words)
     titulo =" ".join(str(x) for x in filtered_words_titulo)
+
+    global diccionario
+    diccionario |= set(body.split())
+
+
 
     return titulo,body
 
@@ -129,3 +137,6 @@ listar()
 c.executemany('''INSERT OR IGNORE INTO noticias(titulo, descripcion) VALUES(?,?)''', listado)
 dbsql.commit()
 print(datetime.now() - startTime)
+
+print("Diccionario: ")
+pp(sorted(diccionario))
